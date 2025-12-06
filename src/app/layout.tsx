5d400d12b58ea26faf138651c8eecfc4057e2357
jowinjohnchemban <jowinchemban@gmail.com>
2025-12-07 00:54:13 +0530

    feat: add @vercel/analytics and integrate into layout for enhanced analytics tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { SpeedInsights } from '@vercel/speed-insights/next';
-
+import { Analytics } from '@vercel/analytics/next';
 
 
 const geistSans = Geist({
@@ -31,7 +31,9 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
